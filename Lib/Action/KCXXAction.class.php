f5c1da1384397307f95dc22205bb9429a85050f6
Connor Weng <foamtea30@gmail.com>
2014-01-13 21:56:04 +0800

    Support adding data to freight_out.

--- Lib/Action/KCXXAction.class.php
+++ Lib/Action/KCXXAction.class.php
@@ -7,10 +7,12 @@ class KCXXAction extends CommonAction {
     );
 
     protected $inModel;
+    protected $outModel;
 
     function __construct() {
         parent::__construct();
         $this->inModel = D('In');
+        $this->outModel = D('Out');
     }
 
     public function kcxx() {
@@ -23,6 +25,17 @@ class KCXXAction extends CommonAction {
         $orderNo = I('order_no');
         $operUserId = session('user')['ID'];
         $rs = $this->inModel->addIn($inDate, $inAmount, $orderNo, $operUserId);
+        $this->ajaxReturn($rs, 'JSON');
+    }
+
+    public function addOut() {
+        $outDate = I('out_date');
+        $outAmount = I('out_amount');
+        $receiveAmount = I('receive_amount');
+        $receiveType = I('receive_type');
+        $orderNo = I('order_no');
+        $operUserId = session('user')['ID'];
+        $rs = $this->outModel->addOut($outDate, $outAmount, $receiveAmount, $receiveType, $orderNo, $operUserId);
         $this->ajaxReturn($rs, 'JSON');
     }
 
---
+++ Lib/Model/OutModel.class.php
@@ -0,0 +1,21 @@
+<?php
+
+class OutModel extends Model {
+
+    protected $fields = array('serious_no', 'out_date', 'out_amount', 'receive_amount', 'receive_type', 'rate', 'order_no', 'build_date', 'oper_user_id');
+
+    public function addOut($outDate, $outAmount, $receiveAmount, $receiveType, $orderNo, $operUserId) {
+        $data['serious_no'] = uniqid();
+        $data['out_date'] = $outDate;
+        $data['out_amount'] = $outAmount;
+        $data['receive_amount'] = $receiveAmount;
+        $data['receive_type'] = $receiveType;
+        $data['order_no'] = $orderNo;
+        $data['build_date'] = date('m/d/Y');
+        $data['oper_user_id'] = $operUserId;
+        return $this->add($data);
+    }
+
+}
+
+?>
